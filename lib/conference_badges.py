00f36f5454d51ac44bcb8772021fc3cd5fdd6d58
Thomas <thomasrains01@gmail.com>
2025-01-18 19:32:07 -0800

    submit

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,18 @@
 def badge_maker(name):
-    return None
+    return (f"Hello, my name is {name}.")
 
 def batch_badge_creator(names):
-    return None
+    return [(f"Hello, my name is {name}.") for name in names]
 
 def assign_rooms(names):
-    return None
+    return [(f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!") for name in names]
 
 def printer(names):
-    return None
+    badge_messages = batch_badge_creator(names)
+    room_messages = assign_rooms(names)
+
+    for message in badge_messages:
+        print(message)
+    for message in room_messages:
+        print(message)
+
